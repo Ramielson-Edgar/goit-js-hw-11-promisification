aa01da1bdc587c192273b01707e1fb4f3c297311
Edgar <jurirulez@gmail.com>
2020-12-03 13:41:32 +0200

    fix

--- src/js/task-3.js
+++ src/js/task-3.js
@@ -10,14 +10,14 @@ const makeTransaction = transaction => {
       const canProcess = Math.random() > 0.3;
 
       if (canProcess) {
-        resolve(logSuccess(transaction.id, delay));
+        resolve({ id: transaction.id, time: delay });
       } else {
-        reject(logError(transaction.id));
+        reject(transaction.id);
       }
     }, delay);
   });
 };
-const logSuccess = (id, time) => {
+const logSuccess = ({ id, time }) => {
   console.log(`Transaction ${id} processed in ${time}ms`);
 };
 
